1a2cd0f3989a5004dec4d288d6493fe8604076fe
Umran <mohammedumran.0416@gmail.com>
2025-09-24 07:38:13 +0100

    adding value from input and displaying to screen using ul, resetting the input value once entered

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const inputBtn = document.getElementById('input-btn')
 const inputEl = document.getElementById('input-el')
+const ulEl = document.getElementById('ul-el')
+
 let myLeads = []
 
 inputBtn.addEventListener('click', () => {
-    console.log('Clicked')
+    myLeads.push(inputEl.value)
+    ulEl.textContent = inputEl.value
+    inputEl.value = ''
 })
+
+for(let i = 0; i < myLeads.length; i++){
+    console.log(myLeads[i])
+    
+}
