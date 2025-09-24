c5cde4581a4d8e85d59a69c4dffeca3b81cfd970
Umran <mohammedumran.0416@gmail.com>
2025-09-24 08:32:53 +0100

    added https before pushing leads so target can open in bland page

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ let listItems = ''
 let myLeads = []
 
 inputBtn.addEventListener('click', () => {
-    myLeads.push(inputEl.value)
+    myLeads.push('https://' + inputEl.value)
+    inputEl.value = ''
     renderLeads()
     
 })
@@ -16,7 +17,14 @@ function renderLeads(){
     listItems = ''
 
     for(let i = 0; i < myLeads.length; i++){
-        listItems += "<li>" + myLeads[i] + "</li>" 
+        
+        listItems += 
+        `<li>
+            <a target='_blank' href=${myLeads[i]}>
+                ${myLeads[i]}
+            </a>
+        </li>`
+
         
     }
 
