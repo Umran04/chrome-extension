c18480f01f3f67242b7f93272b9d6150a4d6c090
Umran <mohammedumran.0416@gmail.com>
2025-09-24 19:27:54 +0100

    rm databse stuff, app is in other repo

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-database.js";
 
-const firebaseConfig = {
-    databaseURL:'https://leads-tracker-app-b62d5-default-rtdb.europe-west1.firebasedatabase.app/'
-}
-
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app)
-
-console.log(app)
-console.log(database)
 
 
 const inputBtn = document.getElementById('input-btn')
